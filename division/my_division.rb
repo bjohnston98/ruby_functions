9354e0d764f1fa0d71dde073590dc73a6a9941fa
Brian Johnston <b_johnston98@yahoo.com>
2016-08-08 13:50:53 -0400

    handling divis by zero

--- division/my_division.rb
+++ division/my_division.rb
@@ -1,5 +1,9 @@
 #my_division
 
 def my_division(num3, num4)
-	num3 / num4
+	if num4 == 0
+		"Can't divide by zero!"
+	else
+		num3 / num4
+	end
 end
--- division/test_div.rb
+++ division/test_div.rb
@@ -14,5 +14,14 @@ class TestDivision<Minitest::Test
 		assert_equal(-7, my_division(14,-2))
 		#
 	end
+
+	def test_div_zero_div_anynum_100
+		assert_equal(0, my_division(0,100))
+	end
+
+	def test_div_anynum_div_zero
+		assert_equal("Can't divide by zero!", my_division(6,0))
+		
+	end
 end
 
