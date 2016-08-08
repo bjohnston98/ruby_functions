e8a0c2090c948c96611a735d1967169398255624
Brian Johnston <b_johnston98@yahoo.com>
2016-08-08 11:36:45 -0400

    mult again

--- multiplication/test_mult.rb
+++ multiplication/test_mult.rb
@@ -5,7 +5,7 @@ require "minitest/autorun"
 require_relative "my_multiplication.rb"
 class TestMultiplication<Minitest::Test
 
-	def test__mult_2_equals_6
+	def test__mult_3_equals_6
 		assert_equal(12, my_multiplication(2,6))
 		#
 	end
